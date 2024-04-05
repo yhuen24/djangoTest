dafd9b7def559d05218d1b9b00b1366344809f52
diana <160737489+6iana@users.noreply.github.com>
2024-04-05 11:34:11 +0100

    Update views.py

--- loginpage/views.py
+++ loginpage/views.py
@@ -1,17 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
+#creating views
+from .models import *
+from .forms import CreateUserForm
 
 
-# def user_login(request):
-   # return render(request, 'registration/login.html')
+def register_view(request):
+    form = CreateUserForm()
 
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account was created for " + form.cleaned_data['username'])
 
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
-        if form.is_valid():
-            # Logic for successful login
-            pass
-    else:
-        form = AuthenticationForm()
-    return render(request, 'registration/login.html', {'form': form})
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    context = {}
+    return render(request, 'registration/login.html', context)
