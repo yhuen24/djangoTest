4afb3af3931f50cb75ed068911efced63da3cba7
diana <160737489+6iana@users.noreply.github.com>
2024-04-24 13:39:19 +0100

    Update urls.py

--- loginpage/urls.py
+++ loginpage/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("main_page/", views.main_view, name="main_page"),
     path("login/", views.login_view, name="user_login"),
-    path("register/", views.register_view, name="register_view")
+    path("register/", views.register_view, name="register_view"),
+    path("admin_login/", views.admin_login_view, name="admin_login")
 ]
