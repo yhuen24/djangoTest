7d8131650dec7f03a292e6d740e33bd1b3ad2fb9
diana <160737489+6iana@users.noreply.github.com>
2024-04-24 13:41:51 +0100

    Update urls.py

--- djangoTest/urls.py
+++ djangoTest/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from testingDjango import views
 
 urlpatterns = [
+    path("main_page/", views.main_view, name="main_page"),
     path('admin/', admin.site.urls),
+    path("user/", include("testingDjango.urls")),
+    path('login', views.login_view, name='login'),
+    path('register', views.register_view, name='register'),
+    path('admin_login', views.admin_login_view, name='adminlogin')
 ]
